7e39287269cab630eadb77943d1e7d1360f224ba
Jose Veliz <jveliz3821@gmail.com>
2019-08-21 22:17:09 -0400

    Add search stock ajax and errors

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,18 +1,40 @@
 class StocksController < ApplicationController
 
   def search
-    if params[:stock].present?
-      @stock = Stock.new_from_lookup(params[:stock])
-      if @stock
-        render 'users/my_portfolio'
-      else
-        flash[:danger] = "You have entered an incorrect symbol"
-        redirect_to my_portfolio_path
-      end
+    if params[:stock].blank?
+      flash.now[:danger] = "You have entered an empty search string"
     else
-      flash[:danger] = "You have entered an empty search string"
-      redirect_to my_portfolio_path
+       @stock = Stock.new_from_lookup(params[:stock])
+       flash.now[:danger] = "You have entered an incorrect symbol" unless @stock
+    end
+    respond_to do |format|
+      format.js { render partial: 'users/result'}
     end
   end
 
 end
+
+#Above code is simplified version of below
+
+    # if params[:stock].present?
+    #   @stock = Stock.new_from_lookup(params[:stock])
+    #   if @stock
+    #     #Use JS to render partial because we're using ajax
+    #     respond_to do |format|
+    #       format.js { render partial: 'users/result'}
+    #     end
+    #   else
+    #     #use .now below to avoid showing errors on following page
+    #     flash.now[:danger] = "You have entered an incorrect symbol"
+    #     #Use JS to render partial because we're using ajax
+    #     respond_to do |format|
+    #       format.js { render partial: 'users/result'}
+    #     end
+    #   end
+    # else
+    #   flash.now[:danger] = "You have entered an empty search string"
+    #   #Use JS to render partial because we're using ajax
+    #   respond_to do |format|
+    #     format.js { render partial: 'users/result'}
+    #   end
+    # end
